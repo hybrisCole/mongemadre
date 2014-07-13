87fc38cfbf37dd9aaf9388e2e4de3c24af1edd41
Abdel Atencio <abdeldw@gmail.com>
2014-07-13 04:44:09 -0600

    amason s3 + blob builder + pojerias

--- app/scripts/directives/htlm2canvas.js
+++ app/scripts/directives/htlm2canvas.js
@@ -1,36 +1,60 @@
 'use strict';
 
 angular.module('mongemadreApp')
-  .directive('htlm2canvas', function ($cookieStore, $timeout, mailer) {
+  .directive('htlm2canvas', function ($timeout) {
     return {
       templateUrl: 'views/cedula.html',
       restrict: 'E',
       link: function (scope, element){
         scope.imageShow = true;
-        localStorage.clickcount = '';
         //scope.imageURL  = '';
         var putin = angular.element(element[0].childNodes[0]);
+
+        AWS.config.update({
+          accessKeyId     : 'AKIAIWOR3KNGZBKQJ3CA', 
+          secretAccessKey : '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'
+        });
+
+        var bucket  = new AWS.S3({params: {Bucket:'blaksun'}});
+        
         $timeout(function() {
             html2canvas(putin,{
               onrendered: function(canvas){
-              //$cookieStore.put('imageURl',canvas.toDataURL());
-              document.body.appendChild(canvas);
-              //angular.element(element[0].childNodes[0]).apend(canvas);
-              console.log(canvas.toDataURL('image/png'));
-              //mailer.submitForm(canvas.toDataURL('image/png'));
-              localStorage.clickcount = canvas.toDataURL('image/png');
+                
+              var params = {
+                Key         : scope.imgName+'.png', 
+                ContentType : 'image/png', 
+                Body        : dataURItoBlob(canvas.toDataURL('image/png'))
+              };
+
+              bucket.putObject(params, function(err, data) {
+                if(err){
+                  console.log(err, err.stack);
+                }else{
+                  console.log(data);
+                }
+              });
+
             }
           });
         }, 0);
 
-        scope.sendme = function(){
-          console.log(localStorage.clickcount);
-          mailer.submitForm(localStorage.clickcount);
-        };
-
-
-        //scope.imageURL  = localStorage.clickcount;
-        //console.log(scope.imageURL);
+        //conver dataURI into a file https://developer.mozilla.org/en-US/docs/Web/API/Blob
+        function dataURItoBlob(dataURI){
+           var byteString, mimestring;
+          if(dataURI.split(',')[0].indexOf('base64') !== -1){
+            byteString = atob(dataURI.split(',')[1]);
+          }else{
+            byteString = decodeURI(dataURI.split(',')[1]);
+          }
+          mimestring = dataURI.split(',')[0].split(':')[1].split(';')[0];
+          
+          var content = [];
+          for (var i = 0; i < byteString.length; i++) {
+            content[i] = byteString.charCodeAt(i);
+          }
+          return new Blob([new Uint8Array(content)], {type: mimestring});
+        }
       }
     };
   });
