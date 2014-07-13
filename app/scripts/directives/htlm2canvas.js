cc5340954ecd1f26a1f6a1ab12abc1af9df41ded
Abdel Atencio <abdeldw@gmail.com>
2014-07-12 20:57:43 -0600

    putin is watching you!

--- app/scripts/directives/htlm2canvas.js
+++ app/scripts/directives/htlm2canvas.js
@@ -3,14 +3,18 @@
 angular.module('mongemadreApp')
   .directive('htlm2canvas', function () {
     return {
+      templateUrl : 'views/cedula.html',
       restrict: 'E',
       link: function (scope, element, attrs) {
+        scope.imageShow = true;
+
         html2canvas(element, {
-  			onrendered: function(canvas) {
-  				console.log(canvas);
-    			document.body.appendChild(canvas);
-  			}
-		});
+  			   onrendered: function(canvas) {
+            scope.imageURL = canvas.toDataURL();
+            scope.imageShow = false;
+            console.log(scope.imageURL);
+  			  }
+		    });
       }
     };
   });
