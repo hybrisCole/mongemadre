989cf9c7897f09a5870fc3820173048cbe8876cc
Alberto Cole <acpii2005@gmail.com>
2014-07-23 01:11:01 -0600

    Arreglos para responsivo

--- app/scripts/controllers/formularioregistro.js
+++ app/scripts/controllers/formularioregistro.js
@@ -18,13 +18,9 @@ angular.module('mongemadreApp')
       $scope.profileImg = data.data.url;
     });
     $scope.submitFormMongeMama = function(){
-      console.log('submitFormMongeMama');
       firebaseService.actualizarUsuario($scope.usuario).then(function(){
-        console.log('firebaseService.actualizarUsuario success');
         facebookService.actualizarFotoPerfil().then(function(){
-          console.log('facebookService.actualizarFotoPerfil success');
           facebookService.actualizarFotoCover($scope.usuario).then(function(){
-            console.log('facebookService.actualizarFotoCover success');
             facebookService.compartirMadreMonge();
             mailer.submitForm($scope.usuario, $scope.profileImg);
             $location.path('/cambiarFotos');
